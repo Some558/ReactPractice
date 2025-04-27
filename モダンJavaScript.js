bdb059fef19141a8896ac3449ca45e66e3d97879
some558 <1641043@gmail.com>
2025-04-28 08:28:30 +0900

    first

---
+++ モダンJavaScript.js
@@ -0,0 +1,37 @@
+/**
+ * const,let等の変数宣言
+ */
+
+// var vali = 'var変数';
+// console.log(vali);
+
+// // var 変数は上書きが可能である。
+// vali = 'aaa';
+// var vali = 'aaaaa';
+// console.log(vali);
+
+// // letは上書き可能
+// let vali = 'var変数';
+// console.log(vali);
+// // letは再宣言不可能
+// let vali = 'vaaaa';
+
+// // constは上書きも再宣言も不可能
+// const vali3 = 'var3';
+// cosnole.log(vali3);
+// vali3 = 'sasss';
+
+// // constで定義したオブジェクトはプロパティの変更が容易である。
+// const val4 = {
+//   name: "じぇけえ",
+//   age = 31
+// };
+// val4.name = "aaaa";
+
+// console.log(val4);
+
+// // constで定義した配列はプロパティの変更が可能
+// const val5 = ["dog", "cat"];
+// val5[0] = "bird";
+// val5.push("moneky");
+// console.log(val5);
